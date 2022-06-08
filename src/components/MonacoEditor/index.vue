54112fe5a7f0b307e8493060adeff3e992e5d0f6
langnang <langnang.chen@outlook.com>
2022-06-08 15:28:02 +0800

    delete contextmenu svg

--- src/components/MonacoEditor/index.vue
+++ src/components/MonacoEditor/index.vue
@@ -1,16 +1,30 @@
 <template>
-  <div ref="monaco"></div>
+  <div ref="monaco" :style="{width:'100%',height:'360px'}"></div>
 </template>
 <script>
+import * as monaco from 'monaco-editor'
 
 export default {
   name: "MonacoEditor",
+  props: {},
   data() {
     return {
       editor: {}
     }
   },
-  mounted: {}
+  mounted() {
+    this.editor = monaco.editor.create(this.$refs.monaco, {
+      value: this.form.text_content,
+      readOnly: false,
+      language: this.form.text_language || 'markdonw',
+      theme: 'vs-dark',
+      wordWrap: true,
+    })
+  },
+  methods: {
+    setOption() { },
+    updateOption() { },
+  }
 }
 </script>
 <style scoped>
