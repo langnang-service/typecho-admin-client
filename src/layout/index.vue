ceb120ad858ac0518a55f5781b16ffd9aea8f672
langnang <langnang.chen@outlook.com>
2022-06-30 12:15:49 +0800

    vue-element-ui

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -3,52 +3,7 @@
     <el-header class="layout-header" style="padding:0 10px;height: 61px; border-bottom: solid 1px #e6e6e6;">
       <el-row :gutter="10">
         <el-col :span="24">
-          <el-menu default-active="layout.menu.active" router collapse style="display: inline-block; vertical-align: top;">
-            <el-submenu index="1">
-              <template slot="title">
-                <i class="el-icon-s-home"></i>
-              </template>
-              <el-menu-item index="/">首页</el-menu-item>
-              <el-menu-item index="/entry">Entry</el-menu-item>
-              <el-menu-item index="/blog">Blog</el-menu-item>
-              <el-menu-item index="/todo">ToDoList</el-menu-item>
-              <el-submenu index="typecho">
-                <template slot="title">Typecho</template>
-                <el-menu-item index="/typecho">Admin</el-menu-item>
-                <el-menu-item index="/template">Template</el-menu-item>
-                <el-menu-item index="/navigation">Navigation</el-menu-item>
-                <el-menu-item index="/public-api">Public APIs</el-menu-item>
-                <el-menu-item index="/snippet">Snippet</el-menu-item>
-                <el-menu-item index="/novel">Novel</el-menu-item>
-              </el-submenu>
-              <el-submenu index="langnang">
-                <template slot="title">Langnang</template>
-
-                <el-menu-item
-                  v-for="(href,key) in {
-              Home:'https://langnang.github.io/',
-              VuePress:'https://langnang.github.io/langnang/',
-              Toolkit:'https://langnang.github.io/toolkit/',
-              'Typecho Web':'https://langnang.github.io/typecho-web/',
-              Card:'https://langnang.github.io/card/',
-              'Audio Player':'https://langnang.github.io/simple-audio-player/',
-              Bookmark:'https://langnang.github.io/simple-bookmark/',
-              Question:'https://langnang.github.io/QuestionAwesome/',
-              ToDoList:'https://langnang.github.io/ToDoList/',
-              Palette:'https://langnang.github.io/Palette/',
-            }"
-                  :key="key"
-                  :index="key"
-                >
-                  <el-link target="_blank" :href="href">{{key}}</el-link>
-                </el-menu-item>
-              </el-submenu>
-              <el-menu-item>理论</el-menu-item>
-              <el-menu-item>实践</el-menu-item>
-              <el-menu-item>工具</el-menu-item>
-              <el-menu-item>资源</el-menu-item>
-            </el-submenu>
-          </el-menu>
+          <LayoutHeaderMenu />
           <el-menu class="hidden-sm-and-down" v-if="layout.menu.visible" :default-active="layout.menu.active" router mode="horizontal" :style="{borderBottom: 'unset',display:'inline-block'}">
             <el-menu-item index="/">首页</el-menu-item>
             <el-menu-item index="/entry">Entry</el-menu-item>
@@ -115,15 +70,18 @@
 <script>
 import { mapState } from 'vuex';
 import LayoutRouterView from "./router-view.vue";
+import LayoutHeaderMenu from './components/header-menu.vue'
 export default {
   name: "Layout",
   components: {
+    LayoutHeaderMenu,
     LayoutRouterView,
   },
   data() {
     return {
       activeIndex: "/",
-      searchInputValue: ''
+      searchInputValue: '',
+      menu: [],
     };
   },
   computed: {
@@ -132,6 +90,10 @@ export default {
     })
   },
   created() {
+    this.$store.dispatch('typecho/content/selectItem', { type: 'branch', slug: 'default' })
+      .then(res => {
+        // this.menu = res.tree
+      })
   },
   methods: {
   },
